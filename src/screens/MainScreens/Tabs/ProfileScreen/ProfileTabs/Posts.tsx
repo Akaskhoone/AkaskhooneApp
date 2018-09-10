4e28bb873235288a42f72f6a67fa0ab778d179e2
Ali.R <1300100eddiecooro2@gmail.com>
2018-09-10 18:16:42 +0430

    Boilerplate added

--- src/screens/MainScreens/Tabs/ProfileScreen/ProfileTabs/Posts.tsx
+++ src/screens/MainScreens/Tabs/ProfileScreen/ProfileTabs/Posts.tsx
@@ -1,27 +1,18 @@
 import images from '@assets/images/feed_images';
 import { Col, Content, Grid, Row } from 'native-base';
 import React, { Component } from 'react';
-import { Dimensions, Image, ScrollView, Text, View } from 'react-native';
+import { Dimensions, FlatList, Image, ScrollView, Text, View } from 'react-native';
+import Bookmarks from 'src/screens/MainScreens/Tabs/ProfileScreen/ProfileTabs/Bookmarks';
 
 const { width, height } = Dimensions.get('window');
 
-export default class Posts extends Component {
+interface Props {
+  data: any;
+}
+export default class Posts extends Component<Props> {
   public render() {
-    const postComponents = images.map((image, index) => (
-      <View
-        key={index}
-        style={{
-          width: width / 3,
-          height: width / 3,
-          marginBottom: 2,
-          paddingLeft: index % 3 !== 0 ? 2 : 0
-        }}>
-        <Image
-          source={image}
-          style={{ flex: 1, alignSelf: 'stretch', width: undefined, height: undefined }}
-        />
-      </View>
-    ));
-    return <View style={{ flexWrap: 'wrap', flexDirection: 'row' }}>{postComponents}</View>;
+    return <FlatList data={this.props.data} renderItem={this.renderItem} />;
   }
+
+  private renderItem = ({ item, index }) => <Bookmark />;
 }
--- src/screens/MainScreens/Tabs/ProfileScreen/ProfileTabs/index.tsx
+++ src/screens/MainScreens/Tabs/ProfileScreen/ProfileTabs/index.tsx
@@ -1,27 +1,51 @@
 import { Content, Tab, Tabs } from 'native-base';
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Dimensions, FlatList, Image, Text, View } from 'react-native';
 import I18n from 'src/utils/i18n';
-import Bookmarks from './Bookmarks';
+import Bookmark from './Bookmark';
 import Posts from './Posts';
 
-export default class ProfileScreen extends Component {
+const { width } = Dimensions.get('window');
+
+interface Props {
+  bookmarks: any;
+  posts: any;
+}
+export default class ProfileScreen extends Component<Props> {
   public render() {
     return (
       <View style={{ flex: 1 }}>
         <Tabs locked={true}>
           <Tab heading={I18n.t('bookmarks')}>
             <Content>
-              <Bookmarks />
+              <FlatList data={this.props.bookmarks} renderItem={this.renderBookmark} />
             </Content>
           </Tab>
           <Tab heading={I18n.t('photos')}>
             <Content>
-              <Posts />
+              <FlatList data={this.props.posts} renderItem={this.renderPost} />
             </Content>
           </Tab>
         </Tabs>
       </View>
     );
   }
+
+  private renderPost = ({ item, index }) => (
+    <View
+      key={index}
+      style={{
+        width: width / 3,
+        height: width / 3,
+        marginBottom: 2,
+        paddingLeft: index % 3 !== 0 ? 2 : 0
+      }}>
+      <Image
+        source={item.image}
+        style={{ flex: 1, alignSelf: 'stretch', width: undefined, height: undefined }}
+      />
+    </View>
+  );
+
+  private renderBookmark = ({ item, index }) => <Bookmark />;
 }
