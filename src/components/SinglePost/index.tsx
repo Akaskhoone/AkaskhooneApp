d5cc2ea0f3064089c7a2906a0e8f942e5f67df71
Ali.R <1300100eddiecooro2@gmail.com>
2018-09-13 19:28:08 +0430

    Minor change

--- src/components/SinglePost/index.tsx
+++ src/components/SinglePost/index.tsx
@@ -25,20 +25,18 @@ export class SinglePost extends Component<Props> {
   public render() {
     const { postId } = this.props;
     return (
-      <View style={{ height: '100%' }}>
-        <Paginator
-          defaultComponent={this.renderDefaultComponent}
-          name={`${postId}_comments`}
-          type="comments"
-          url={`/social/posts/${postId}/comments`}
-          ListHeaderComponent={this.renderHeader}
-          renderItem={this.renderItem}
-        />
-      </View>
+      <Paginator
+        defaultComponent={this.renderDefaultComponent}
+        name={`post${postId}_comments`}
+        type="comments"
+        url={`/social/posts/${postId}/comments`}
+        ListHeaderComponent={this.renderHeader}
+        renderItem={this.renderItem}
+      />
     );
   }
   private renderDefaultComponent = () => {
-    return <View />;
+    return this.renderHeader();
   };
   private renderHeader = () => {
     return <PostCard postId={this.props.postId} />;
